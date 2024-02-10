4cf5b12e728b287e26c9b008b2935e822983db39
adarsh <01fe22bca014@kletech.ac.in>
2024-02-10 11:13:05 +0530

    This is sept

---
+++ department.java
@@ -0,0 +1,13 @@
+public class department {
+      public static void  department(int id)
+      {
+          if(id==101)
+          {
+              System.out.println("bca");     
+          }
+          else if(id==102)
+          {
+              System.out.println("mca");
+          }
+      }
+  }
