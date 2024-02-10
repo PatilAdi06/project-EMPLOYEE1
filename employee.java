40b08256b6092dac9759984a5c237ef1c5ba502a
adarsh <01fe22bca014@kletech.ac.in>
2024-02-10 11:10:57 +0530

    This is employee

---
+++ employee.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class employee extends department
+{
+    public static void main(String arg[])
+    {
+        Scanner n=new Scanner(System.in);
+        String name;
+        String date;
+        int id;
+        department d=new department();
+        System.out.println("Enter Employee name :");
+        name=n.nextLine();
+        System.out.println("Enter date of joining of Employee :");
+        date=n.next();
+        System.out.println("Enter Department id :");
+        id =n.nextInt();
+        System.out.println("\nnEmployee dtails");
+        System.out.println("Name :"+name);
+        System.out.println("Joining Date :"+date);
+        System.out.println("Department Name :");
+        d.department(id);
+
+
+    }
+}
+
